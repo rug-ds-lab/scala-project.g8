afadc6f562f288f0ad3454c77bf7a8f4d0552d5e
lazovik <lazovik@gmail.com>
2014-07-28 19:15:56 +0200

    remove test dependency (for a moment, to be added later, most probably through some kind of parent extension)

--- src/main/g8/src/test/scala/$group__packaged$/$name__packaged$/$name__Camel$Spec.java
+++ src/main/g8/src/test/scala/$group__packaged$/$name__packaged$/$name__Camel$Spec.java
@@ -1,12 +1,7 @@
 package $group$.$name$
 
-import org.scalatest.FunSpec
+//import org.scalatest.FunSpec
 
-class $name;format="Camel"$Spec extends FunSpec {
-    describe("Adding 1 to 1") {
-        it("should equals 2"){
-            assert(1+1 == 2)
-        }
-    }
+class $name;format="Camel"$Spec /* extends FunSpec */ {
 }
 
