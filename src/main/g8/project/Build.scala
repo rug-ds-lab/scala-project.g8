becf810f99e9ddb6db6b5ddebfa150a08790a24f
lazovik <lazovik@gmail.com>
2014-12-17 15:35:34 +0100

    new projects, even standalone ones are created as subprojects

--- src/main/g8/project/Build.scala
+++ src/main/g8/project/Build.scala
@@ -3,7 +3,10 @@ import sbt.Keys._
 import rugds.sbt._
 
 object ScalaBuild extends Build {
-  val project = defineProject(scalaProject, "$name$") settings (
+  val $name$Main = mainProject
+    .aggregate($name$Project)
+
+  val $name$Project = defineProject(scalaProject, "$name$") settings (
     
   )
 }
