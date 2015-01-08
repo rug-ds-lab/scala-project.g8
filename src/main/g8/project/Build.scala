03900211404541cc416642298d327518c5154305
lazovik <lazovik@gmail.com>
2015-01-08 13:04:00 +0100

    fix: project names with '-' symbol

--- src/main/g8/project/Build.scala
+++ src/main/g8/project/Build.scala
@@ -3,10 +3,10 @@ import sbt.Keys._
 import rugds.sbt._
 
 object ScalaBuild extends Build {
-  lazy val $name;format="camel"$Main = mainProject
-    .aggregate($name;format="camel"$Project)
+  lazy val $name;format="start,decap,word"$Main = mainProject
+    .aggregate($name;format="start,decap,word"$Project)
 
-  lazy val $name;format="camel"$Project = defineProject(scalaProject, "$name$") settings (
+  lazy val $name;format="start,decap,word"$Project = defineProject(scalaProject, "$name$") settings (
     
   )
 }
