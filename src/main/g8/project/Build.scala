ce6aa1d0aaa3451faf7798f9fb9f80fe4b342d95
lazovik <lazovik@gmail.com>
2015-03-26 14:53:32 +0100

    add mainClass definition

--- src/main/g8/project/Build.scala
+++ src/main/g8/project/Build.scala
@@ -7,6 +7,6 @@ object ScalaBuild extends Build {
     .aggregate($name;format="camel"$Project)
 
   lazy val $name;format="camel"$Project = defineProject(scalaProject, "$name$") settings (
-    
+    mainClass in Compile = Some("$organization__packaged$.$name__word$.$name$")
   )
 }
