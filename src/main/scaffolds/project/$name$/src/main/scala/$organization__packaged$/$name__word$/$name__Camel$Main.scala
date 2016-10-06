dcce09ed3e5f80304779afcbb7b1ba0928755130
lazovik <lazovik@gmail.com>
2016-10-06 13:25:26 +0200

    g8 scaffold: project

---
+++ src/main/scaffolds/project/$name$/src/main/scala/$organization__packaged$/$name__word$/$name__Camel$Main.scala
@@ -0,0 +1,8 @@
+package $organization$.$name;format="word-only"$
+
+object $name;format="Camel"$Main {
+
+  def main(args: Array[String]) {
+    println("Hello World!!!")
+  }
+}
---
+++ src/main/scaffolds/project/$name$/src/test/scala/$organization__packaged$/$name__word$/$name__Camel$Spec.scala
@@ -0,0 +1,7 @@
+package $organization$.$name;format="word-only"$
+
+//import org.scalatest.FunSpec
+
+class $name;format="Camel"$Spec /* extends FunSpec */ {
+}
+
